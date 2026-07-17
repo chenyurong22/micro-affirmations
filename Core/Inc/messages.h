5123962ab7b2376bfc2e65e1a0dbd260eb420668
Isaac Pawley <75295202+i-Saac-IV@users.noreply.github.com>
2026-07-17 13:14:25 +1200

    chore: added messages file

---
+++ Core/Inc/messages.h
@@ -0,0 +1,16 @@
+/*
+
+File:   messages.h
+Author: Isaac Pawley
+
+*/
+
+#ifndef INC_MESSAGES_H_
+#define INC_MESSAGES_H_
+
+#include <stddef.h>
+
+extern const char *messages[];
+extern size_t number_of_messages;
+
+#endif /* INC_MESSAGES_H_ */
---
+++ Core/Src/messages.c
@@ -0,0 +1,16 @@
+/*
+
+File:   messages.c
+Author: Isaac Pawley
+
+*/
+
+#include "messages.h"
+
+const char *messages[] = {
+    "uAffirmations - A microcontroller-based affection-giving MACHINE!",
+    "Whoops, it looks like you forgot to add you own messages!",
+    "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+};
+
+size_t number_of_messages = sizeof(messages) / sizeof(messages[0]);
